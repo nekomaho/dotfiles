995774c0d10131279c1db12ee23d310bc8a50c64
nekomaho <nekosatoru@gmail.com>
2017-11-05 22:47:11 +0900

    split Rakefile class

---
+++ lib/default_shell_settings.rb
@@ -0,0 +1,21 @@
+class DefaultShellSetting
+  ETC_SHELL="/etc/shell"
+
+  extend Rake::DSL
+  def self.setting(shell_pass)
+    return if already_setting?(shell_pass)
+    password = get_password
+    sh("echo #{password} | sudo -S sh -c 'echo #{shell_pass} >> #{ETC_SHELL}}'")
+    sh("chsh -s #{shell_pass}")
+  end
+
+  def self.already_setting?(shell_pass)
+    /#{shell_pass}/ === File.open(ETC_SHELL).read
+  end
+
+  def self.get_password
+    STDIN.noecho { Readline.readline("Password: ").tap{ puts } }
+  end
+end
+
+
