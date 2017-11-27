cb61f7a5bb1a450e22bd8f0614de83abbbf4e5d2
nekomaho <nekosatoru@gmail.com>
2017-11-28 00:46:17 +0900

    fix brew execute when use bundler

--- lib/homebrew_install.rb
+++ lib/homebrew_install.rb
@@ -1,21 +1,31 @@
-require "open3"
+require 'open3'
 
 class  HomebrewInstall
   extend Rake::DSL
 
-  def self.install(app)
-    check(app) ? sh("brew install #{app}") : puts("skip install #{app}")
-  end 
+  class << self
+    def install(app)
+      Bundler.with_original_env do
+        check(app) ? sh("brew install #{app}") : puts("skip install #{app}")
+      end
+    end
 
-  def self.check(app)
-    Open3.capture3("brew info #{app}")[0] =~ /Not installed/
-  end 
+    def check(app)
+      Bundler.with_original_env do
+        Open3.capture3("brew info #{app}")[0] =~ /Not installed/
+      end
+    end
 
-  def self.upgrade(app)
-    upgrade_check(app) ? sh("brew upgrade #{app}") : puts("skip upgrade #{app}")
-  end 
+    def upgrade(app)
+      Bundler.with_original_env do
+        upgrade_check(app) ? sh("brew upgrade #{app}") : puts("skip upgrade #{app}")
+      end
+    end
 
-  def self.upgrade_check(app)
-    Open3.capture3("brew outdated #{app}")[0] =~ /#{app}/
+    def upgrade_check(app)
+      Bundler.with_original_env do
+        Open3.capture3("brew outdated #{app}")[0] =~ /#{app}/
+      end
+    end
   end
 end
