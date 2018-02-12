3826ec668267bad81b77b36345f453c2f95e7a56
nekomaho <nekosatoru@gmail.com>
2018-02-12 23:25:45 +0900

    add configuration file interface to initialize argument

--- lib/config_settings.rb
+++ lib/config_settings.rb
@@ -3,16 +3,15 @@ require "yaml"
 class ConfigSettings
   attr_reader :dotfiles_dir,:src,:app,:vim_clone_path,:qfixhowm_path
 
-  def initialize
-    @config = YAML.load_file("config.yml").map{ |k,v| [k.to_sym, v] }.to_h
+  def initialize(config_file="config.yml")
+    @config = YAML.load_file(config_file).map{ |k,v| [k.to_sym, v] }.to_h
     @home = "#{ENV["HOME"]}"
     @dotfiles_dir = "#{@home}/#{@config[:DOTFILE_PATH]}"
     @src = "#{@home}/src"
     @app = "#{@home}/app"
     @vim_clone_path = "#{@src}/vim"
     @qfixhowm_path = "#{@home}/.qfixhowm"
-
-  end 
+  end
 
   def zshrc_home_path
     "#{@home}/.zshrc"
