df33eff4d536d9a6cd6387f54437f740329f43ee
nekomaho <nekosatoru@gmail.com>
2018-04-12 20:18:17 +0900

    Fix /etc/shells path :bug:

--- lib/default_shell_settings.rb
+++ lib/default_shell_settings.rb
@@ -1,11 +1,11 @@
 class DefaultShellSetting
-  ETC_SHELL="/etc/shell"
+  ETC_SHELL="/etc/shells"
 
   extend Rake::DSL
   def self.setting(shell_pass)
     return if already_setting?(shell_pass)
     password = get_password
-    sh("echo #{password} | sudo -S sh -c 'echo #{shell_pass} >> #{ETC_SHELL}}'")
+    sh("echo #{password} | sudo -S sh -c 'echo #{shell_pass} >> #{ETC_SHELL}'")
     sh("chsh -s #{shell_pass}")
   end
 
