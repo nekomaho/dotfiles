de24ff381ab3573f10b53fab462c2f680fdec70c
nekomaho <nekosatoru@gmail.com>
2018-06-16 22:52:58 +0900

    Refactor ConfigSettings class

--- lib/config_settings.rb
+++ lib/config_settings.rb
@@ -3,10 +3,10 @@ require "yaml"
 class ConfigSettings
   attr_reader :dotfiles_dir,:src,:app,:vim_clone_path,:qfixhowm_path
 
-  def initialize(config_file="config.yml")
-    @config = YAML.load_file(config_file).map{ |k,v| [k.to_sym, v] }.to_h
+  def initialize(input_config_file=nil)
+    @input_config_file = input_config_file
     @home = "#{ENV["HOME"]}"
-    @dotfiles_dir = "#{@home}/#{@config[:DOTFILE_PATH]}"
+    @dotfiles_dir = "#{@home}/#{config[:DOTFILE_PATH]}"
     @src = "#{@home}/src"
     @app = "#{@home}/app"
     @vim_clone_path = "#{@src}/vim"
@@ -22,7 +22,7 @@ class ConfigSettings
   end
 
   def zsh_install_path
-    @config[:ZSH_INSTALL_PATH]
+    config[:ZSH_INSTALL_PATH]
   end
 
   def vimrc_home_path
@@ -34,7 +34,17 @@ class ConfigSettings
   end
 
   def vim_install_path
-    @config[:VIM_INSTALL_PATH]
+    config[:VIM_INSTALL_PATH]
+  end
+
+  private
+
+  def config_file
+    @config_file ||= @input_config_file || "config.yml"
+  end
+
+  def config
+    @config ||= YAML.load_file(config_file).map{ |k,v| [k.to_sym, v] }.to_h
   end
 end
 
