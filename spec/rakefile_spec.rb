4ef6f1df9afd39e2e931ed86ef8ecf02ec985db0
nekomaho <nekosatoru@gmail.com>
2018-06-27 19:51:30 +0900

    Add rakefile tests
    add vimrc tests and upgrade tasks test

--- spec/rakefile_spec.rb
+++ spec/rakefile_spec.rb
@@ -192,5 +192,117 @@ describe 'Rakefile tests' do
         expect(FileTest.symlink?(conf.zshrc_home_path)).to be_truthy
       end
     end
+
+    describe 'copy vimrc to vimrc_path' do
+      after do
+        File.delete(conf.vimrc_path)
+      end
+
+      it 'is exist .vimrc to vimrc_path' do
+        @rake[conf.vimrc_path].invoke
+        expect(FileTest.exist?(conf.vimrc_path)).to be_truthy
+      end
+    end
+
+    describe 'create symblic link to vimrc_home_path to vimrc_path' do
+      before do
+        FileUtils.touch(conf.vimrc_path)
+      end
+
+      after do
+        File.delete(conf.vimrc_home_path)
+        File.delete(conf.vimrc_path)
+      end
+
+      it 'is symbolic link of vimrc_path' do
+        @rake[conf.vimrc_home_path].invoke
+        expect(FileTest.symlink?(conf.vimrc_home_path)).to be_truthy
+      end
+    end
+  end
+
+  describe 'upgrade' do
+    describe 'upgrade all app task' do
+      describe 'upgrade all app dependency' do
+        it 'is dependent make_dir, zsh_upgrade, vim_upgrade, ag_install, global_upgrade' do
+          expect(@rake['upgrade:all'].sources).to include 'rake:make_dir'
+          expect(@rake['upgrade:all'].sources).to include 'zsh_upgrade'
+          expect(@rake['upgrade:all'].sources).to include 'vim_upgrade'
+          expect(@rake['upgrade:all'].sources).to include 'rake:ag_install'
+          expect(@rake['upgrade:all'].sources).to include 'global_upgrade'
+        end
+      end
+    end
+
+    describe 'upgrade related zsh' do
+      describe 'zsh upgrade dependency' do
+        it 'is dependent zsh, zsh_completions, .zshrc' do
+          expect(@rake['upgrade:zsh_upgrade'].sources).to include 'rake:zsh'
+          expect(@rake['upgrade:zsh_upgrade'].sources).to include 'rake:zsh_completions'
+          expect(@rake['upgrade:zsh_upgrade'].sources).to include "#{conf.zshrc_home_path}"
+        end
+      end
+    end
+
+    describe 'upgrade related global' do
+      describe 'global upgrade dependency' do
+        it 'is dependent global, exuberant_ctags and pygments' do
+          expect(@rake['upgrade:global_upgrade'].sources).to include 'rake:global'
+          expect(@rake['upgrade:global_upgrade'].sources).to include 'rake:exuberant_ctags'
+          expect(@rake['upgrade:global_upgrade'].sources).to include 'rake:pygments'
+        end
+      end
+    end
+
+    describe 'dein upgrade' do
+      describe 'dein upgrade dependency' do
+        it 'is dependent vim' do
+          expect(@rake['upgrade:dein_upgrade'].sources).to include 'vim'
+        end
+      end
+
+      describe 'dein main upgrade task' do
+        describe 'dein upgrade when update' do
+          before do
+            # only test this task not dependency task
+            @rake['upgrade:dein_upgrade'].clear_prerequisites
+          end
+
+          it 'is install dein' do
+            expect(@rake['upgrade:dein_upgrade'].invoke[0].call).to eq "#{conf.dotfiles_dir}"
+          end
+        end
+      end
+    end
+
+    describe 'vim upgrade' do
+      describe 'vim upgrade dependency' do
+        it 'is dependent .vimrc and lua' do
+          expect(@rake['upgrade:vim'].sources).to include "#{conf.vimrc_home_path}"
+          expect(@rake['upgrade:vim'].sources).to include 'rake:lua'
+        end
+      end
+
+      describe 'vim main upgrade task' do
+        before do
+          # only test this task not dependency task
+          @rake['upgrade:vim'].clear_prerequisites
+        end
+
+        describe 'vim upgrade when update' do
+          it 'is update vim' do
+            allow(File).to receive(:exist?).and_return(true)
+            expect(@rake['upgrade:vim'].invoke[0].call).to eq "#{conf.vim_clone_path}"
+          end
+        end
+
+        describe 'vim upgrade when install' do
+          it 'is build  vim' do
+            allow(File).to receive(:exist?).and_return(false)
+            expect(@rake['upgrade:vim'].invoke[0].call).to eq "#{conf.vim_clone_path}"
+          end
+        end
+      end
+    end
   end
 end
