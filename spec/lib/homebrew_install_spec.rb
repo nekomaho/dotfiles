10f69aba4c8d332883f4c40d9bcebbc893fd4d86
nekomaho <nekosatoru@gmail.com>
2018-09-04 23:56:10 +0900

    Add HomebrewInstall.upgrade spec

--- spec/lib/homebrew_install_spec.rb
+++ spec/lib/homebrew_install_spec.rb
@@ -32,4 +32,32 @@ describe 'Homebrew install tests' do
       it { is_expected.to be_falsy }
     end
   end
+
+  describe "#upgrade" do
+    subject(:upgrade){ HomebrewInstall.upgrade('dummy') }
+
+    context "when need upgrade" do
+      before do
+        allow(HomebrewInstall).to receive(:sh)
+        allow(HomebrewInstall).to receive(:upgrade_check).and_return(true)
+      end
+
+      it do
+        subject
+        expect(HomebrewInstall).to have_received(:sh).with('brew upgrade dummy')
+      end
+    end
+
+    context "when do not need upgrade" do
+      before do
+        allow(HomebrewInstall).to receive(:puts)
+        allow(HomebrewInstall).to receive(:upgrade_check).and_return(false)
+      end
+
+      it do
+        subject
+        expect(HomebrewInstall).to have_received(:puts).with('skip upgrade dummy')
+      end
+    end
+  end
 end
