d84e0fe664e8419f6410ccdbdac9970ccc8a2081
nekomaho <nekosatoru@gmail.com>
2018-09-05 23:17:53 +0900

    Add already done settings case in default_shell_settings_spec

--- spec/lib/default_shell_settings_spec.rb
+++ spec/lib/default_shell_settings_spec.rb
@@ -4,7 +4,7 @@ describe 'DefaultShellSetting tests' do
   describe "#setting" do
     subject(:setting) {DefaultShellSetting.setting('/dummy/pass')}
  
-    context "when need install" do
+    context "when need setting" do
       before do
         allow(DefaultShellSetting).to receive(:sh)
         allow(DefaultShellSetting).to receive(:get_password).and_return('password')
@@ -15,6 +15,18 @@ describe 'DefaultShellSetting tests' do
         subject
         expect(DefaultShellSetting).to have_received(:sh).with('echo password | sudo -S sh -c \'echo /dummy/pass >> /etc/shells\'').once
         expect(DefaultShellSetting).to have_received(:sh).with('chsh -s /dummy/pass').once
+      end
+    end
+
+    context "when already done setting" do
+      before do
+        allow(DefaultShellSetting).to receive(:get_password)
+        allow(DefaultShellSetting).to receive(:already_setting?).and_return(true)
+      end
+
+      it do
+        subject
+        expect(DefaultShellSetting).not_to have_received(:get_password)
       end
     end
   end
