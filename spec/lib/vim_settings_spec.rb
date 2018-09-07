d0fc635df76cb60cb3811c6f6b0c87fb42a2434d
nekomaho <nekosatoru@gmail.com>
2018-09-07 20:19:43 +0900

    Add vim_make method spec of VimInstall

--- spec/lib/vim_settings_spec.rb
+++ spec/lib/vim_settings_spec.rb
@@ -69,7 +69,7 @@ describe 'VimInstall tests' do
   end
 
   describe "#dein" do
-    subject(:update) {VimInstall.dein('/vim/clone/path', '/vim/app/path')}
+    subject(:dein) {VimInstall.dein('/vim/clone/path', '/vim/app/path')}
 
     context "when dein exists already" do
       before do
@@ -95,6 +95,31 @@ describe 'VimInstall tests' do
         subject
         expect(VimInstall).to have_received(:sh).with('git clone https://github.com/Shougo/dein.vim /vim/clone/path/dein')
       end
+    end
+  end
+
+  describe "#vim_make" do
+    subject(:vim_make) {VimInstall.vim_make('/vim/app/path')}
+    let(:expect_options) do
+      "--prefix=/vim/app/path"\
+      " --enable-luainterp=yes"\
+      " --enable-rubyinterp=yes"\
+      " --enable-multibyte"\
+      " --enable-terminal"\
+      " --enable-darwin"\
+      " --enable-python3interp=dynamic"\
+      " --enable-pythoninterp=dynamic"\
+      " --with-lua-prefix=/usr/local"
+    end
+
+    before do
+      allow(VimInstall).to receive(:configure)
+      allow(VimInstall).to receive(:make)
+    end
+
+    it do
+      subject
+      expect(VimInstall).to have_received(:configure).with(expect_options)
     end
   end
 end
