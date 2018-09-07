c5be37c8b6d55f3a83556765052da0ba40f3f9f6
nekomaho <nekosatoru@gmail.com>
2018-09-07 19:33:23 +0900

    Add test method spec of VimInstall

--- spec/lib/vim_settings_spec.rb
+++ spec/lib/vim_settings_spec.rb
@@ -32,4 +32,19 @@ describe 'VimInstall tests' do
       expect(VimInstall).to have_received(:make_install)
     end
   end
+
+  describe "#test" do
+    subject(:install) {VimInstall.test('/vim/clone/path')}
+
+    before do
+      allow(VimInstall).to receive(:cd)
+      allow(VimInstall).to receive(:make_test)
+    end
+
+    it do
+      subject
+      expect(VimInstall).to have_received(:cd).with('/vim/clone/path')
+      expect(VimInstall).to have_received(:make_test)
+    end
+  end
 end
