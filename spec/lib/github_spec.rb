7d20913045c95acc5de4570817806c853301680e
nekomaho <nekosatoru@gmail.com>
2019-01-02 22:45:41 +0900

    Add github spec

--- spec/lib/github_spec.rb
+++ spec/lib/github_spec.rb
@@ -1,9 +1,9 @@
 require './spec/base'
 
 describe "Github test" do
-  let(:github) { Github.new('rails/rails', protocol: :git) }
-
   describe "#clone_dir" do
+    let(:github) { Github.new('rails/rails', protocol: :git) }
+
     subject { github.clone_dir('/hoge/hoge') }
     it { is_expected.to eq '/hoge/hoge' }
   end
@@ -15,9 +15,22 @@ describe "Github test" do
       allow(github).to receive(:sh)
     end
 
-    it do
-      subject
-      expect(github).to have_received(:sh).with('git clone git@github.com:rails/rails ')
+    context "when direct clone_dir" do
+      let(:github) { Github.new('rails/rails', protocol: :ssh, clone_dir: '/hoge/hoge') }
+
+      it do
+        subject
+        expect(github).to have_received(:sh).with('git clone https://github.com/rails/rails /hoge/hoge')
+      end
+    end
+
+    context "when not direct clone_dir" do
+      let(:github) { Github.new('rails/rails', protocol: :git) }
+
+      it do
+        subject
+        expect(github).to have_received(:sh).with('git clone git@github.com:rails/rails ')
+      end
     end
   end
 end
