870ae789e5c51b95176948dc2b62025de40eacee
nekomaho <nekosatoru@gmail.com>
2019-03-23 17:25:18 +0900

    Add brewapp.yml

--- spec/lib/homebrew/rake_task_generator_spec.rb
+++ spec/lib/homebrew/rake_task_generator_spec.rb
@@ -2,16 +2,16 @@ require './spec/base'
 
 RSpec.describe HomebrewInstall::RakeTaskGenerator do
   describe '.load' do
-    subject { HomebrewInstall::RakeTaskGenerator.load }
+    subject { HomebrewInstall::RakeTaskGenerator.load.to_h }
 
     context 'exists setting yaml' do
       context 'yaml is correct gramar' do
-        let(:expect_hash) do
-          [vim: {}, 'universal-ctags': { install_name: "--HEAD universal-ctags/universal-ctags/universal-ctags" }, zsh: {}]
+        let(:expect_array) do
+          [ { vim: {} }, { 'universal-ctags': { install_name: "--HEAD universal-ctags/universal-ctags/universal-ctags" } }, { zsh: {}}]
         end
 
         it 'load yaml setting to hash' do
-          is_expected.to eq expect_hash
+          is_expected.to match_array expect_array
         end
       end
     end
